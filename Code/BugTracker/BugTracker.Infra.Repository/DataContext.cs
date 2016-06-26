a3540bc0daa98edb9824c1101f3922172863c737
Gustavo <gustavomelloti@gmail.com>
2016-06-26 20:40:15 -0300

    feat(repository): criar configuração do entity (dataContext.cs), e adicionar mapeamentos para entidade User.

---
+++ Code/BugTracker/BugTracker.Infra.Repository/DataContext.cs
@@ -0,0 +1,38 @@
+﻿using BugTracker.Domain.Entity;
+using BugTracker.Infra.Repository.Map;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Infra.Repository
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("Conn"){ }
+
+        public DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            this.Configuration.ProxyCreationEnabled = false;
+            this.Configuration.LazyLoadingEnabled = false;
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties<string>()
+             .Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));
+           
+            modelBuilder.Configurations.Add(new UserMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Code/BugTracker/BugTracker.Infra.Repository/DataEntity/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using BugTracker.Domain.Entity;
+using BugTracker.Domain.Interface.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Infra.Repository.DataEntity
+{
+    public class UserRepository : IUserRepository
+    {
+        public User FindById(int id)
+        {
+            using (var db = new DataContext())
+            {
+                return db.User.AsNoTracking().FirstOrDefault(_ => _.Id == id);
+            }
+        }
+    }
+}
