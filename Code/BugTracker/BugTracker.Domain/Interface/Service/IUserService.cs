8db4ac0a10c4893ed874b665775d37f96deb0f59
Gustavo <gustavomelloti@gmail.com>
2016-06-26 19:45:19 -0300

    feat(domain): criar artefatos necessários para a entidade User.

---
+++ Code/BugTracker/BugTracker.Domain/Interface/Service/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using BugTracker.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Domain.Interface.Service
+{
+    public interface IUserService
+    {
+        User FindById(int id);
+    }
+}
---
+++ Code/BugTracker/BugTracker.Domain/Service/UserService.cs
@@ -0,0 +1,26 @@
+﻿using BugTracker.Domain.Entity;
+using BugTracker.Domain.Interface.Repository;
+using BugTracker.Domain.Interface.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Domain.Service
+{
+    public class UserService : IUserService
+    {
+        IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            this._userRepository = userRepository;
+        }
+
+        public User FindById(int id)
+        {
+            return this._userRepository.FindById(id);
+        }
+    }
+}
