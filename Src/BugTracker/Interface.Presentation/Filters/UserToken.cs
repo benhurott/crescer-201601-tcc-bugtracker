2c076d73a7076903778477a30e3d26ef6bff94f2
ViniciusAudibert <vini.audibert@gmail.com>
2016-06-27 20:16:17 -0300

    feat(UserToken): adicionar authorize attribute do usuario.

--- Src/BugTracker/Interface.Presentation/Filters/UserToken.cs
+++ Src/BugTracker/Interface.Presentation/Filters/UserToken.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Interface.Presentation.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace Interface.Presentation.Filters
 {
@@ -10,12 +12,22 @@ namespace Interface.Presentation.Filters
     {
         protected override bool AuthorizeCore(HttpContextBase httpContext)
         {
-            return base.AuthorizeCore(httpContext);
+            return UserSessionService.IsLogged;
         }
 
         public override void OnAuthorization(AuthorizationContext filterContext)
         {
-            base.OnAuthorization(filterContext);
+            bool isLogged = this.AuthorizeCore(filterContext.HttpContext);
+
+            if (!isLogged)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                                   new RouteValueDictionary
+                                   {
+                                       { "action", "SignIn" },
+                                       { "controller", "Login" }
+                                   });
+            }
         }
     }
 }
