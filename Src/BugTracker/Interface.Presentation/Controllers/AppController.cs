cb862c71bed8f5a15fc8c129829ffd05ba0929a2
Fernando Schneider Leandro <nando.leandro@hotmail.com>
2016-06-27 17:27:15 -0300

    feat(ModelView): implementar modelview para aplicações.

---
+++ Src/BugTracker/Interface.Presentation/Controllers/AppController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Interface.Presentation.Controllers
+{
+    public class AppController : Controller
+    {
+        // GET: App
+        public ActionResult AppList()
+        {
+            var ap1 = new Models.ApplicationModel();
+            ap1.Title = "FakeBook";
+
+            var ap2 = new Models.ApplicationModel();
+            ap2.Title = "Xml Reader";
+            
+            return View();
+        }
+    }
+}
