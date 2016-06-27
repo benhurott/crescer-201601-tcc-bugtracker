73a4adc19124d013631f46e54cc2ffec6326440d
ViniciusAudibert <vini.audibert@gmail.com>
2016-06-27 19:57:05 -0300

    feat(ApplicationViewModel): adicionar aplicação para view model.

---
+++ Src/BugTracker/Interface.Presentation/Models/Application/ApplicationViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Interface.Presentation.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Interface.Presentation.Models.Application
+{
+    public class ApplicationViewModel
+    {
+        public int IDApplication { get; private set; }
+        public String Title { get; private set; }
+        public String Description { get; private set; }
+        public String Url { get; private set; }
+        public String Image { get; private set; }
+        public String SpecialTag { get; private set; }
+        public LoggedUserViewModel User { get; private set; }
+
+        public ApplicationViewModel(int idApplication,String title,String description,String url,String image,String specialTag,LoggedUserViewModel user)
+        {
+            this.IDApplication = idApplication;
+            this.Title = title;
+            this.Description = description;
+            this.Url = url;
+            this.Image = image;
+            this.SpecialTag = specialTag;
+            this.User = user;
+        }
+    }
+}
