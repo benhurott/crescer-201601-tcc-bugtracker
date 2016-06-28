399e4cc654b542285d06606415a861db9e962a7f
Gustavo <gustavomelloti@gmail.com>
2016-06-27 23:25:46 -0300

    feat(repository): adicionar mapeamentos incompletos.

---
+++ Src/BugTracker/BugTracker.Infra.Repository/Map/BugTrackerMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio = BugTracker.Domain.Entity;
+
+namespace BugTracker.Infra.Repository.Map
+{
+    public class BugTrackerMap : EntityTypeConfiguration<Dominio.BugTracker>
+    {
+        public BugTrackerMap()
+        {
+            ToTable("BugTracker");
+            HasKey(_ => _.IDBugTracker);
+        }
+    }
+}
---
+++ Src/BugTracker/BugTracker.Infra.Repository/Map/BugTrackerNavigationMap.cs
@@ -0,0 +1,19 @@
+﻿using BugTracker.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Infra.Repository.Map
+{
+    public class BugTrackerNavigationMap : EntityTypeConfiguration<BugTrackerNavigation>
+    {
+        public BugTrackerNavigationMap()
+        {
+            ToTable("BugTrackerNavigation");
+            HasKey(_ => _.IDBugTrackerNavigation);
+        }
+    }
+}
