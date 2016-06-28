253afe39b7e11d2f5529ef51cb438e5de422a9f4
Gustavo <gustavomelloti@gmail.com>
2016-06-27 23:38:46 -0300

    fix(presentation): adicionar arquivo deletado por acidente.

---
+++ Src/BugTracker/Interface.Presentation/Services/BugTrackerServiceInjection.cs
@@ -0,0 +1,17 @@
+﻿using BugTracker.Domain.Service;
+using BugTracker.Infra.Repository.DataEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Interface.Presentation.Services
+{
+    public static class BugTrackerServiceInjection
+    {
+        public static BugTrackerService Create()
+        {
+            return new BugTrackerService(new BugTrackerRepository());
+        }
+    }
+}
