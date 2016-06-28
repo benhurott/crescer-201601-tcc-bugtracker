f00809f6c85f57c87269b9969e783ddd08e2bc8b
Gustavo <gustavomelloti@gmail.com>
2016-06-27 21:13:39 -0300

    feat(repository): adicionar métodos de repositório da classe Application e UserRecovery.

---
+++ Src/BugTracker/BugTracker.Infra.Repository/DataEntity/UserRecoveryRepository.cs
@@ -0,0 +1,30 @@
+﻿using BugTracker.Domain.Entity;
+using BugTracker.Domain.Interface.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Infra.Repository.DataEntity
+{
+    public class UserRecoveryRepository : IUserRecoveryRepository
+    {
+        public void Add(UserRecovery userRecovery)
+        {
+            using (var db = new DataContext())
+            {
+                db.Entry<UserRecovery>(userRecovery).State = System.Data.Entity.EntityState.Added;
+                db.SaveChanges();
+            }
+        }
+
+        public UserRecovery FindByEmail(string email)
+        {
+            using (var db = new DataContext())
+            {
+                return db.UserRecovery.FirstOrDefault(_ => _.RequestUser.Email == email);
+            }
+        }
+    }
+}
