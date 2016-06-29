3346c12fae3c6c3d3129dffd45f7999ad6369eb9
Gustavo Silva de Mello <gustavo.mello@cwinet.local>
2016-06-29 17:30:11 -0300

    feat(presentation): adicionar layout para edição.

---
+++ Src/BugTracker/Interface.Presentation/Models/User/UserEditAccountViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Interface.Presentation.Models.User
+{
+    public class UserEditAccountViewModel
+    {
+        public int? Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DisplayName("E-mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public String Image { get; set; }
+
+        [PasswordPropertyText(true)]
+        [RegularExpression(@"^(?=.*\d)(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$", ErrorMessage = "Password must have: minimun length of 8 characters, a number, a low case word and a hight case word.")]
+        [DataType(DataType.Password)]
+        [StringLength(100)]
+        public String Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [DisplayName("Confirm Password")]
+        [Compare("Password", ErrorMessage = "Password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
