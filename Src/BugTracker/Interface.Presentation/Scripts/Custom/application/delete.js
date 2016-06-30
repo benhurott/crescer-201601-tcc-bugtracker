ee9b51e03b8a260d3af0ac9e5e17e58c10b2e611
Fernando Schneider Leandro <nando.leandro@hotmail.com>
2016-06-30 16:37:45 -0300

    feat(Script): adicionar script de deleçao

---
+++ Src/BugTracker/Interface.Presentation/Scripts/Custom/application/delete.js
@@ -0,0 +1,16 @@
+﻿'use strict'
+
+$(function () {
+
+    $('.delete').on("click", function () {
+
+        var id = $(this).attr('id');
+
+        $('.delete').popover({
+            placement: 'left',
+            html: 'true',
+            title: 'Sure?',
+            content: 'Are you sure to delete this application?' + '<a href="/Application/DeleteApp/' + id + '">Yes</a>'
+        });
+    });
+});
