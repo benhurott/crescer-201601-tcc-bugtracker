a9ecaeb222a2ecabced251fe5e3c133d39f907f0
ViniciusAudibert <vini.audibert@gmail.com>
2016-07-01 06:38:19 -0300

    feat(Bundles): adicionar todos os scripts e css no BundleConfig e nas views .

--- Src/BugTracker/Interface.Presentation/App_Start/BundleConfig.cs
+++ Src/BugTracker/Interface.Presentation/App_Start/BundleConfig.cs
@@ -10,18 +10,51 @@ namespace Interface.Presentation.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //SCRIPTS
 
-            bundles.Add(new ScriptBundle("~/bundles/formularios-validacoes").Include(
+            bundles.Add(new ScriptBundle("~/bundles/validations-form").Include(
                         "~/Scripts/jquery.validate.js",
                         "~/Scripts/jquery.validate.unobtrusive.js",
-                        "~/Scripts/jquery.unobtrusive-ajax.min.js",
-                        "~/Scripts/Custom/overrides-validacao.js"));
+                        "~/Scripts/jquery.unobtrusive-ajax.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                        "~/Content/bootstrap.css",
-                        "~/Content/Custom/principal.css"));
+            bundles.Add(new ScriptBundle("~/bundles/application").Include(
+                        "~/Scripts/Custom/application/delete.js",
+                        "~/Scripts/Custom/model/bug-tracker-model.js",
+                        "~/Scripts/Custom/view/bug-tracker-view.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/account").Include(
+                        "~/Scripts/Custom/User/account.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/layout").Include(
+                        "~/Scripts/jquery-1.10.2.js",
+                        "~/Scripts/modernizr-2.6.2.js",
+                        "~/Scripts/jquery.validate.js",
+                        "~/Scripts/jquery.validate.unobtrusive.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.js",
+                        "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/layout-home").Include(
+                        "~/Scripts/jquery-1.10.2.js",
+                        "~/Scripts/modernizr-2.6.2.js",
+                        "~/Scripts/jquery.validate.js",
+                        "~/Scripts/jquery.validate.unobtrusive.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/Custom/layout/layoutHome.js"));
+
+            //CSS
+
+            bundles.Add(new StyleBundle("~/Content/layout-home").Include(
+                        "~/Content/bootstrap-sandstone.css",
+                        "~/Content/Custom/layout-home.css",
+                        "~/content/Custom/login.css",
+                        "~/content/Custom/index.css"));
+
+            bundles.Add(new StyleBundle("~/Content/layout").Include(
+                        "~/Content/bootstrap-sandstone.css",
+                        "~/Content/Custom/panel.css",
+                        "~/Content/Custom/layout.css",
+                        "~/Content/Custom/new-home.css"));
         }
     }
 }
