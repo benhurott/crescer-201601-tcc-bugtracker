110c43a8b86311aace5df85b33130370d367ed1e
ViniciusAudibert <vini.audibert@gmail.com>
2016-07-02 15:02:02 -0300

    fix(delete): ajeitar bug do duplo clique na deleção de aplicações.

--- Src/BugTracker/Interface.Presentation/Scripts/Custom/application/delete.js
+++ Src/BugTracker/Interface.Presentation/Scripts/Custom/application/delete.js
@@ -2,15 +2,10 @@
 
 $(function () {
 
-    $('.delete').on("click", function () {
-
-        var id = $(this).attr('id');
-
-        $('.delete').popover({
-            placement: 'left',
-            html: 'true',
-            title: 'Sure?',
-            content: 'Are you sure to delete this application?' + '<a href="/Application/DeleteApp/' + id + '">Yes</a>'
-        });
+    $('.delete').popover({
+        placement: 'left',
+        html: 'true',
+        title: 'Sure?',
+        content: 'Are you sure to delete this application?' + '<a href="/Application/DeleteApp/' + $('.delete').attr('id') + '">Yes</a>'
     });
 });
