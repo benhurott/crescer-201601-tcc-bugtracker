8b56b3ed8beb8f3fc46f8b65b1177458b884fe29
Fernando Schneider Leandro <Fernando Schneider Leandro>
2016-07-02 14:16:44 -0300

    Test(AppController): implementar teste para controller de applications (andamento).

---
+++ Src/BugTracker/BugTracker.Interface.Presentation.Test/UnitTests/ApplicationControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Interface.Presentation.Controllers;
+
+namespace BugTracker.Interface.Presentation.Test
+{
+    [TestClass]
+    public class ApplicationControllerTest
+    {
+
+        private ApplicationController applicationController;
+
+        [TestMethod]
+        public void RegisterAppToNewApp()
+        {
+
+        }
+    }
+}
