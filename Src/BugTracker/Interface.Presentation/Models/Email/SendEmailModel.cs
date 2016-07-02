d85b2eecbc3d5ecae8870f9bb2129d0e9d514c97
ViniciusAudibert <vini.audibert@gmail.com>
2016-07-02 14:28:29 -0300

    feat(Layout): adicionar layout home refeito, adicionar telas de contado e home.

---
+++ Src/BugTracker/Interface.Presentation/Models/Email/SendEmailModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Interface.Presentation.Models.Email
+{
+    public class SendEmailModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
--- Src/BugTracker/Interface.Presentation/Scripts/Custom/layout/layoutHome.js
+++ Src/BugTracker/Interface.Presentation/Scripts/Custom/layout/layoutHome.js
@@ -1,5 +1,28 @@
 ﻿$(function () {
 
-    $('.navbar-link[data-target-id="' + document.title + '"]').addClass('active-tab');
+    $('.nav-link[data-target-id="' + document.title + '"]').addClass('active');
 
 })
+
+jQuery(function ($) {
+
+    //Ajax contact
+    var form = $('.contact-form');
+    form.submit(function () {
+        $this = $(this);
+        $.post($(this).attr('action'), function (data) {
+            $this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
+        }, 'json');
+        return false;
+    });
+
+    //Goto Top
+    $('.gototop').click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("body").offset().top
+        }, 500);
+    });
+    //End goto top		
+
+});
