a9ac799f90f9b03a350a173ac9aa393cb2913d4a
ViniciusAudibert <vini.audibert@gmail.com>
2016-07-03 00:07:53 -0300

    feat(Registrar Aplicação): adicionar botão de imagem.

--- Src/BugTracker/Interface.Presentation/Scripts/Custom/layout/layoutHome.js
+++ Src/BugTracker/Interface.Presentation/Scripts/Custom/layout/layoutHome.js
@@ -8,7 +8,25 @@
         $information.css('display', 'block');
     }
 
+    $("#imgInput").change(function () {
+        readURL(this);
+    });
+
 })
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#imgShow').attr('src', e.target.result);
+            $('#project-img').slideDown('slow');
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
 
 jQuery(function ($) {
 
